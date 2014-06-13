d9fa72e81cfa6c1ea06ae63d83c7cc40758f9630
jc <emclab2011@gmail.com>
2014-06-13 16:19:57 -0500

    JC: changed form put to get in routes.rb for search_result.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ BillOfMaterialx::Engine.routes.draw do
   resources :boms do
     collection do
       get :search
-      put :search_results
+      get :search_results
       #get :stats
-      #put :stats_results 
+      #get :stats_results 
     end
   end
   root :to => 'boms#index'
